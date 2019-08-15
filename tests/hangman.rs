6ee45f7e5d125a9272a68d5ff7df4f77e0867e42
Bader Alshaya <alshayabader@gmail.com>
2019-08-14 21:38:41 -0700

    Added unit tests.

--- tests/hangman.rs
+++ tests/hangman.rs
@@ -1,1 +1,12 @@
+use hangman;
 
+#[test]
+fn test_empty_hidden_word() {
+    let target = hangman::RoundProgress {
+        hidden_word: Vec::new(),
+        status: hangman::RoundStatus::Ongoing,
+        points: 0,
+        failed_attempts: 0,
+    };
+    assert_eq!(hangman::get_hidden_word(&target), String::from(""));
+}
